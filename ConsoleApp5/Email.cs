f8c54467bf380e3910aeb65cc1568f0fe85b37da
NuNes <NuNes@DESKTOP-RKLAV38>
2023-04-21 17:18:51 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp5/Email.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Email : IMensagem
+    {
+        public void EnviarMensagem(string destinatario, string mensagem)
+        {
+            // Código para enviar e-mail
+            Console.WriteLine($"Enviando e-mail para {destinatario}: {mensagem}");
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IMensagem email = new Email();
+            email.EnviarMensagem("fulano@gmail.com", "Olá, Fulano!");
+
+            IMensagem sms = new SMS();
+            sms.EnviarMensagem("999999999", "Olá, tudo bem?");
+
+            Console.ReadKey();
+
+        }
+    }
+}
